5daa88484b75585660a246a985046f30dcb341b2
lucaa126 <163129939+lucaa126@users.noreply.github.com>
2025-11-05 20:42:16 +0000

    fine grafica e logica pagina2

--- site/src/app/pagina1/pagina1.ts
+++ site/src/app/pagina1/pagina1.ts
@@ -13,148 +13,88 @@ import { ProductList } from '../product-list/product-list';
 export class Pagina1 {
   products: Product[] = [
     {
-      name: 'CERCHI LED',
-      price: 100,
-      description: 'DEC-TEC LIGHTING',
-      imageUrl: 'https://www.wiredledlight.com/wired/images/family/family-25.jpg'
+      name: 'cristalli di MDMA pura:',
+      price: 21,
+      description: 'solitamente in tonalità bianca, beige o marroncina, può essere ingerita o sciolta in acqua.',
+      imageUrl: 'https://antidroga.interno.gov.it/wp-content/uploads/2019/05/Metanfetamina_0376024d0.jpg'
     },
     {
-      name: 'DISEGNI A LED',
-      price: 150,
-      description: 'DEC-TEC LIGHTING',
-      imageUrl: 'https://www.wiredledlight.com/wired/images/family/family-26.jpg'
+      name: 'Compresse di ecstasy',
+      price: 19,
+      description: 'pillole di vari colori e loghi; spesso contengono MDMA mescolata con altre sostanze .',
+      imageUrl: 'https://media-assets.wired.it/photos/615da292a1d512b2960a780b/16:9/w_1280,c_limit/wired_placeholder_dummy.png'
     },
     {
-      name: 'DIVA',
-      price: 100,
-      description: 'DOWNLIGHTING',
-      imageUrl: 'https://www.wiredledlight.com/wired/images/family/family-1.jpg'
+      name: 'MDA',
+      price: 17,
+      description: 'chimicamente simile, più stimolante e meno empatica.',
+      imageUrl: 'https://www.dea.gov/sites/default/files/styles/slide/public/2018-07/ecstacy-03.jpg?itok=fhCUi2hv'
     },
     {
-      name: 'EGGS',
-      price: 150,
-      description: 'DOWNLIGHTING',
-      imageUrl: 'https://www.wiredledlight.com/wired/images/family/family-14.jpg'
+      name: 'MDEA',
+      price: 17,
+      description: 'variante più sedativa e di durata minore.',
+      imageUrl: 'https://antidroga.interno.gov.it/wp-content/uploads/2019/05/MDMA.jpg'
     },
     {
-      name: 'FOCUS',
-      price: 200,
-      description: 'DOWNLIGHTING',
-      imageUrl: 'https://www.wiredledlight.com/wired/images/family/family-12.jpg'
+      name: 'PMMA / PMA',
+      price: 16,
+      description: 'analoghi pericolosi talvolta spacciati per ecstasy, con effetti tossici elevati.',
+      imageUrl: 'https://cdn.adf.org.au/media/images/drug-macro-pf-mdma-03-930x620.width-1524.jpg'
     },
     {
-      name: 'HALO',
-      price: 200,
-      description: 'DEC-TEC LIGHTING',
-      imageUrl: 'https://www.wiredledlight.com/wired/images/family/family-29.jpg'
+      name: 'Cocaina cloridrato',
+      price: 90,
+      description: 'polvere bianca solubile in acqua, comunemente sniffata o disciolta per iniezione.',
+      imageUrl: 'https://www.sanpatrignano.org/wp-content/uploads/2020/08/cocaina.jpg'
     },
     {
-      name: 'HUNTER',
-      price: 300,
-      description: 'DOWNLIGHTING',
-      imageUrl: 'https://www.wiredledlight.com/wired/images/family/family-20.jpg'
+      name: 'Crack',
+      price: 30,
+      description: 'ottenuto riscaldando cocaina cloridrato con bicarbonato o ammoniaca; si fuma e agisce rapidamente.',
+      imageUrl: 'https://www.welfarepost.it/wp-content/uploads/2025/09/gemini_generated_image_w8vtytw8vtytw8vt.webp'
     },
     {
-      name: 'KUBIK',
-      price: 150,
-      description: 'DOWNLIGHTING',
-      imageUrl: 'https://www.wiredledlight.com/wired/images/family/family-17.jpg'
+      name: 'Pasta base / basuco',
+      price: 20,
+      description: 'prodotto intermedio meno raffinato, contiene residui di solventi e impurità.',
+      imageUrl: 'https://www.saponificiovaresino.com/svstore/wp-content/uploads/2020/01/palmacocco-econat-soap-base.png'
     },
     {
-      name: 'LEDWOODS',
-      price: 300,
-      description: 'DEC-TEC LIGHTING',
-      imageUrl: 'https://www.wiredledlight.com/wired/images/family/family-28.jpg'
+      name: 'Freebase',
+      price: 25,
+      description: 'versione purificata della base libera, simile al crack ma ottenuta con etere etilico.',
+      imageUrl: 'https://www.akzent-luzern.ch/pr%C3%A4vention/news/458/image-thumb__458__slideImage/crack.29b915a5.jpg'
     },
     {
-      name: 'LED_0 TRACKLIGHT',
-      price: 150,
-      description: 'RETAIL LIGHTING',
-      imageUrl: 'https://www.wiredledlight.com/wired/images/family/family-21.jpg'
+      name: 'Polvere di metanfetamina',
+      price: 40,
+      description: 'può essere sniffata o iniettata.',
+      imageUrl: 'https://media-assets.wired.it/photos/615dacbe5a8281a8f96ca4bf/16:9/w_1040,h_585,c_limit/214888_web.jpg'
     },
     {
-      name: 'LED_O 220V',
-      price: 150,
-      description: 'DOWNLIGHTING',
-      imageUrl: 'https://www.wiredledlight.com/wired/images/family/family-23.jpg'
+      name: 'Crystal meth',
+      price: 43,
+      description: 'forma cristallina, trasparente o azzurrina, solitamente fumata; è la più potente e a lunga durata.',
+      imageUrl: 'https://www.boardpreprecovery.com/wp-content/uploads/2017/09/what-is-methamphetamine-1200x600.jpeg'
     },
     {
-      name: 'LED_O WALL WASHER',
-      price: 150,
-      description: 'DOWNLIGHTING',
-      imageUrl: 'https://www.wiredledlight.com/wired/images/family/family-22.jpg'
+      name: 'Yaba',
+      price: 26,
+      description: 'compresse diffuse nel Sud-est asiatico, miscelate con caffeina',
+      imageUrl: 'https://www.tribuneindia.com/sortd-service/imaginary/v22-01/jpg/large/high?url=dGhldHJpYnVuZS1zb3J0ZC1wcm8tcHJvZC1zb3J0ZC9tZWRpYTRkMzA2ZGMwLTRlN2UtMTFlZi05ZmFhLWFiNzg5M2FlZWVhYy5qcGc='
     },
     {
-      name: 'LINDA',
-      price: 150,
-      description: 'DEC-TEC LIGHTING',
-      imageUrl: 'https://www.wiredledlight.com/wired/images/family/family-27.jpg'
+      name: 'Desoxyn',
+      price: 20,
+      description: 'metanfetamina legale usata in rarissimi casi come farmaco (per ADHD o obesità, sotto stretta prescrizione medica).',
+      imageUrl: 'https://www.ashwoodrecovery.com/wp-content/uploads/2022/02/pills-header11.jpg'
     },
     {
-      name: 'LINEA LENS',
-      price: 350,
-      description: 'ARCHITECTURAL LIGHTING',
-      imageUrl: 'https://www.wiredledlight.com/wired/images/family/family-5.jpg'
-    },
-    {
-      name: 'LINEE LED',
-      price: 250,
-      description: 'ARCHITECTURAL LIGHTING',
-      imageUrl: 'https://www.wiredledlight.com/wired/images/family/family-6.jpg'
-    },
-    {
-      name: 'LOIS',
-      price: 250,
-      description: 'ARCHITECTURAL LIGHTING',
-      imageUrl: 'https://www.wiredledlight.com/wired/images/family/family-10.jpg'
-    },
-    {
-      name: 'LOTO',
-      price: 150,
-      description: 'ARCHITECTURAL LIGHTING',
-      imageUrl: 'https://www.wiredledlight.com/wired/images/family/family-9.jpg'
-    },
-    {
-      name: 'LUDO',
-      price: 300,
-      description: 'ARCHITECTURAL LIGHTING',
-      imageUrl: 'https://www.wiredledlight.com/wired/images/family/family-7.jpg'
-    },
-    {
-      name: 'MICRO',
-      price: 100,
-      description: 'DOWNLIGHTING',
-      imageUrl: 'https://www.wiredledlight.com/wired/images/family/family-16.jpg'
-    },
-    {
-      name: 'ONEFORALL',
-      price: 200,
-      description: 'DOWNLIGHTING',
-      imageUrl: 'https://www.wiredledlight.com/wired/images/family/family-15.jpg'
-    },
-    {
-      name: 'OTLY+DONNA',
-      price: 50,
-      description: 'DEC-TEC LIGHTING',
-      imageUrl: 'https://www.wiredledlight.com/wired/images/family/family-30.jpg'
-    },
-    {
-      name: 'PRISMILED',
-      price: 350,
-      description: 'DEC-TEC LIGHTING',
-      imageUrl: 'https://www.wiredledlight.com/wired/images/family/family-24.jpg'
-    },
-    {
-      name: 'R_LINE',
-      price: 130,
-      description: 'ARCHITECTURAL LIGHTING',
-      imageUrl: 'https://www.wiredledlight.com/wired/images/family/family-11.jpg'
-    },
-    {
-      name: 'TIN LED OPAL',
-      price: 100,
-      description: 'DOWNLIGHTING',
-      imageUrl: 'https://www.wiredledlight.com/wired/images/family/family-19.jpg'
+      name: 'Ice / glass',
+      price: 70,
+      description: 'versioni ad alta purezza di crystal meth',
+      imageUrl: 'https://www.heart.org/en/-/media/Images/News/2019/August-2019/0821MethCVD_SC.jpg?sc_lang=en'
     },
     
   ];
